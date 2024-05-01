f76be02f108a5d8edc6ee5eaa52ba0bcf3cb41fb
sunriseaigo <sunrise96208@gmail.com>
2024-05-01 03:26:37 -0400

    add feature that fetch the balance of address

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const { getKeypairFromEnvironment } = require("@solana-developers/helpers");
+const { Connection, clusterApiUrl, PublicKey } = require("@solana/web3.js");
 
 require("dotenv/config")
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
+const connection = new Connection(clusterApiUrl("devnet"));
+console.log(`✅ Connected!`)
+
+const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
+
+connection.getBalance(address).then(balance => {
+    console.log(`The balance of the account at ${address} is ${balance} lamports`)
+    console.log(`✅ Finished!`)
+})
+
